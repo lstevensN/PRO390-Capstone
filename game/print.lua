9f818a52457af9c8fffb572d8a2f62109787ea94
lstevensN <lstevens@student.neumont.edu>
2024-09-29 11:53:06 -0600

    Added main.lua

---
+++ game/print.lua
@@ -0,0 +1,3 @@
+function printWally()
+    return 'Wall-e'
+end
---
+++ main.lua
@@ -0,0 +1,46 @@
+--[[
+    Your love2d game start here
+]]
+
+
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
+function love.load()
+    require("game.print")
+
+    wally = printWally()
+
+    -- init something here ...
+    love.window.setTitle('Hello love2d!')
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    -- ...
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
+
+function love.update(dt)
+    -- change some values based on your actions
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    -- draw your stuff here
+    love.graphics.print('Welcome to the Love2d world!', 10, 10)
+    love.graphics.print(wally, 10, 100)
+end
+
