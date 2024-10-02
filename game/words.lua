e27d20e47229bab0a81552e1a02349bf2dd0f325
lstevensN <lstevens@student.neumont.edu>
2024-10-02 13:32:07 -0600

    Added G, H, I, & Y word lists

--- game/words.lua
+++ game/words.lua
@@ -1,9 +1,12 @@
-local a = require "game.lists.a"
-local b = require "game.lists.b"
-local c = require "game.lists.c"
-local d = require "game.lists.d"
-local e = require "game.lists.e"
-local f = require "game.lists.f"
+local a  = require "game.lists.a"
+local b  = require "game.lists.b"
+local c  = require "game.lists.c"
+local d  = require "game.lists.d"
+local e  = require "game.lists.e"
+local f  = require "game.lists.f"
+local g  = require "game.lists.g"
+local h  = require "game.lists.h"
+local iy = require "game.lists.iy"
 
 local function ValidateWord(word)
     local startLetter = word:sub(1, 1)
@@ -20,6 +23,12 @@ local function ValidateWord(word)
         for i = 1, #e do if e[i] == word then return true end end
     elseif startLetter == 'f' then
         for i = 1, #f do if f[i] == word then return true end end
+    elseif startLetter == 'g' then
+        for i = 1, #g do if g[i] == word then return true end end
+    elseif startLetter == 'h' then
+        for i = 1, #h do if h[i] == word then return true end end
+    elseif startLetter == 'i' or startLetter == 'y' then
+        for i = 1, #iy do if iy[i] == word then return true end end
     end
 
     return false
