d6cc1a6fceef8580cf65af21b23349a196e6b0ee
lstevensN <lstevens@student.neumont.edu>
2024-10-02 12:31:31 -0600

    Added D, E, & F word lists

--- game/words.lua
+++ game/words.lua
@@ -1,6 +1,9 @@
 local a = require "game.lists.a"
 local b = require "game.lists.b"
 local c = require "game.lists.c"
+local d = require "game.lists.d"
+local e = require "game.lists.e"
+local f = require "game.lists.f"
 
 local function ValidateWord(word)
     local startLetter = word:sub(1, 1)
@@ -11,6 +14,12 @@ local function ValidateWord(word)
         for i = 1, #b do if b[i] == word then return true end end
     elseif startLetter == 'c' then
         for i = 1, #c do if c[i] == word then return true end end
+    elseif startLetter == 'd' then
+        for i = 1, #d do if d[i] == word then return true end end
+    elseif startLetter == 'e' then
+        for i = 1, #e do if e[i] == word then return true end end
+    elseif startLetter == 'f' then
+        for i = 1, #f do if f[i] == word then return true end end
     end
 
     return false
