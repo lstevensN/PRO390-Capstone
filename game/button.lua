dd964b7b1f7c1a1834ea0b082a2a358d7b6b17c3
lstevensN <lstevens@student.neumont.edu>
2024-10-17 10:28:04 -0600

    RESOLUTION SOLUTION :D
    hehe

--- game/button.lua
+++ game/button.lua
@@ -14,8 +14,8 @@ function Button(xpos, ypos, w, h, onclick)
             local x, y = love.mouse.getPosition()
 
             if self.pressed == false and
-            (x >= self.x - self.width / 2 and x <= self.x - self.width / 2 + self.width) and
-            (y >= self.y - self.height / 2 and y <= self.y - self.height / 2 + self.height) then
+            (x >= (self.x - self.width / 2) * XScaleFactor and x <= (self.x - self.width / 2 + self.width) * XScaleFactor) and
+            (y >= (self.y - self.height / 2) * YScaleFactor and y <= (self.y - self.height / 2 + self.height) * YScaleFactor) then
                 self.onClick()
                 self.pressed = true
             end
@@ -24,7 +24,7 @@ function Button(xpos, ypos, w, h, onclick)
     end
 
     self.draw = function ()
-        love.graphics.rectangle("fill", self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
+        love.graphics.rectangle("fill", (self.x - self.width / 2) * XScaleFactor, (self.y - self.height / 2) * YScaleFactor, self.width * XScaleFactor, self.height * YScaleFactor)
     end
 
     return self
--- main.lua
+++ main.lua
@@ -1,31 +1,14 @@
 local state
 local rs = require "resolution_solution" ---@type ResolutionSolution
 
+local screenX, screenY = 800, 600
+
+XScaleFactor = 0
+YScaleFactor = 0
+
 local mouseX, mouseY = 0, 0
 
-rs.conf({
-    game_width = 1000,
-    game_height = 800,
-    scale_mode = rs.ASPECT_MODE
-})
-
-rs.setMode(rs.game_width, rs.game_height, {resizable = true})
-
-local game_canvas = love.graphics.newCanvas(rs.get_game_size())
-
-local mouse_getPosition = love.mouse.getPosition
-love.mouse.getPosition = function ()
-    local width, height = love.graphics.getDimensions()
-    local original_width = 1000
-    local original_height = 800
-    local sx, sy = width / original_width, height / original_height
-    local x, y = mouse_getPosition()
-    x = x / sx
-    y = y / sy
-    return x, y
-end
-
-function love.resize(w, h) rs.resize(w, h) end
+function love.resize(w, h) end
 
 function love.load()
     require("game.state")
@@ -34,6 +17,7 @@ function love.load()
     state.load()
 
     love.window.setTitle('PRO-390 Capstone')
+    love.window.setMode(screenX, screenY, {resizable = true})
 end
 
 -- Main Game Loop
@@ -42,21 +26,18 @@ function love.update(dt)
     state.update(dt)
 
     mouseX, mouseY = love.mouse.getPosition()
+
+    XScaleFactor = love.graphics.getWidth() / screenX
+    YScaleFactor = love.graphics.getHeight() / screenY
 end
 
-function love.draw()
-    love.graphics.setCanvas(game_canvas)
-    love.graphics.clear(0, 0, 0, 1)
-    
+function love.draw()   
     love.graphics.print('Here goes nothing! (^v^)', 10, 10)
     love.graphics.print("mouse x: "..tostring(mouseX), 10, 40)
     love.graphics.print("mouse y: "..tostring(mouseY), 10, 60)
 
+    love.graphics.print("scale x: "..tostring(XScaleFactor), 10, 90)
+    love.graphics.print("scale y: "..tostring(YScaleFactor), 10, 110)
+
     state.draw()
-
-    love.graphics.setCanvas()
-
-    rs.push()
-        love.graphics.draw(game_canvas)
-    rs.pop()
 end
